4b79b951e902d8358f33763c7dea0deb8891b4f0
IvanLipskyi <lips.ivan5@gmail.com>
2019-06-09 20:42:49 +0300

    made on 9th June

--- src/Generics/DocumentProcessorUtil.java
+++ src/Generics/DocumentProcessorUtil.java
@@ -9,4 +9,22 @@ public class DocumentProcessorUtil {
     StringData convert(T data){
         return new StringData(++ID_COUNTER, data.getType(), data);
     }
+    static <T> void build (Storable storeObj, T data){
+        storeObj = new Storable() {
+            @Override
+            public Object read() {
+                return null;
+            }
+
+            @Override
+            public void write(Object data) {
+
+            }
+
+            @Override
+            public Type getType() {
+                return null;
+            }
+        };
+    }
 }
