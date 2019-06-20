9af641e99f055078fa1c0b108cc21000c4276964
Maxim Shiryaev <maksim155555@gmail.com>
2019-06-20 19:50:42 +0300

    20.06

---
+++ src/Consumer/Test.java
@@ -0,0 +1,13 @@
+package Consumer;
+
+/**
+ * Created by Brainacad4 on 20.06.2019.
+ */
+public class Test {
+    public static void main(String[] args) {
+/*static void createManyUsers: (int size, Supplier<?extends Employee>S){
+        *for (int i = 0, i<size, i++){
+        s.get();
+    }*/
+    }
+}
