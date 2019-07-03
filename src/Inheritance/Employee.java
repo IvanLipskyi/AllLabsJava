8ef4fc4d67d83a5f3c1be21877343ec55aad97ff
IvanLipskyi <lips.ivan5@gmail.com>
2019-07-03 11:33:18 +0300

    03.07added

---
+++ src/Inheritance/Employee.java
@@ -0,0 +1,75 @@
+package Inheritance;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Employee extends Human implements Serializable {
+    private String acceptedDate;
+    private double salary;
+    private boolean isWorking;
+
+    void comeOnWork(){
+        System.out.println("I'm on work today");
+    }
+    void haveLunch(){
+        System.out.println("I'm eating");
+    }
+    void doWork(){
+        System.out.println("I'm working");
+    }
+
+    public Employee(String name, String bday, String phone, String acceptedDate, double salary, boolean isWorking) {
+        super(name, bday, phone);
+        this.acceptedDate=acceptedDate;
+        this.salary = salary;
+        this.isWorking=isWorking;
+    }
+
+    public String getAcceptedDate() {
+        return acceptedDate;
+    }
+
+    public void setAcceptedDate(String acceptedDate) {
+        this.acceptedDate = acceptedDate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public boolean isWorking() {
+        return isWorking;
+    }
+
+    public void setWorking(boolean working) {
+        isWorking = working;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0 &&
+                isWorking == employee.isWorking &&
+                Objects.equals(acceptedDate, employee.acceptedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(acceptedDate, salary, isWorking);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "acceptedDate='" + acceptedDate + '\'' +
+                ", salary=" + salary +
+                ", isWorking=" + isWorking +
+                '}';
+    }
+}
---
+++ src/Inheritance/Supermarket.java
@@ -0,0 +1,29 @@
+package Inheritance;
+
+public class Supermarket {
+    public Supermarket(String name, String adress) {
+        this.name = name;
+        this.adress = adress;
+    }
+    private String name;
+    private String adress;
+    Manager man1;
+    Cashier cas1;
+    Guard gua1;
+    Customer cus1;
+
+    void open(){
+        System.out.println("The shop is opened");
+    }
+    void getLunch(){
+        System.out.println("We are going on lunch");
+    }
+    void sellGoods(){
+        System.out.println("We are selling goods");
+    }
+
+    public static void main(String[] args) {
+        Supermarket silpo = new Supermarket("Silpo", "Sobornyi street, 104");
+
+    }
+}
