92663f87bf7a57bf9e1e002be67a8dbbefeb47f5
IvanLipskyi <lips.ivan5@gmail.com>
2019-07-04 11:17:26 +0300

    04.07 IO streams full

---
+++ src/IOStreams/FileCopy.java
@@ -0,0 +1,17 @@
+
+package IOStreams;
+import java.io.*;
+
+public class FileCopy {
+    public static void main(String[] args) {
+        try(FileInputStream fis = new FileInputStream("C://Users//User//IdeaProjects//hello_world//testing.txt");
+            FileOutputStream fos = new FileOutputStream("C://Users//User//IdeaProjects//hello_world//testing_new.txt"))
+        {
+            byte[]buffer = new byte[fis.available()];
+            fis.read(buffer, 0, buffer.length);
+            fos.write(buffer, 0, buffer.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
