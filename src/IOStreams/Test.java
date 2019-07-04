0d46bbd05d0e63a478978688366c7b030c7ed621
IvanLipskyi <lips.ivan5@gmail.com>
2019-07-04 11:00:14 +0300

    04.07 IO streams were added

---
+++ src/IOStreams/Test.java
@@ -0,0 +1,22 @@
+//package IOStreams;
+//import java.io.*;
+//import java.util.Arrays;
+//
+//public class Test {
+//    static void someMethod(String s){
+//        System.out.println(s);
+//    }
+//    static void fileManager(File file){
+//        if(file.isDirectory()){
+//            for(File fff: file.listFiles()){
+//
+//            }
+//        }
+//    }
+//    public static void main(String[] args) {
+//File file = new File ("C//Users//User//IdeaProjects//hello_world");
+//someMethod("the name of directory: " + file.getName());
+//someMethod("The path is: " + file.getPath());
+//fileManager(file);
+//    }
+//}
